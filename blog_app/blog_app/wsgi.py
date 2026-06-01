395bc7e4bcc60d643f63b3e17a43f1ab4475c7ad
sushantapatra <development.experiment@gmail.com>
2026-06-01 22:27:57 +0530

    first commit

--- blog_app/blog_app/wsgi.py
+++ blog_app/blog_app/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for core project.
+WSGI config for blog_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_app.settings')
 
 application = get_wsgi_application()
